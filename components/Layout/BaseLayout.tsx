6acb4b6fae9a22ad97de9218d27b9ea74317db7a
Harry Nguyen <harry.nguyen2@techvify.com.vn>
2022-04-29 13:07:42 +0700

    upload Meta ogImage

--- components/Layout/BaseLayout.tsx
+++ components/Layout/BaseLayout.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import HeaderHome from "../Common/Header/HeaderHome";
-
+import Meta from "../../styles/assets/images/newsDes/HotNews.png";
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <meta property="og:image" content={Meta.src} />
+      </Head>
       <HeaderHome />
       <main>{children}</main>
     </>
