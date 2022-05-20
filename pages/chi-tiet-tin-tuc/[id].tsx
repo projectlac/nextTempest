3940057d2a8589478605b50df91324486a11a027
projectlac <dft1711198@gmail.com>
2022-05-20 22:01:43 +0700

    fix fallback

--- pages/chi-tiet-tin-tuc/[id].tsx
+++ pages/chi-tiet-tin-tuc/[id].tsx
@@ -50,7 +50,7 @@ export async function getStaticPaths() {
   }));
   // We'll pre-render only these paths at build time.
   // { fallback: false } means other routes should 404.
-  return { paths, fallback: true };
+  return { paths, fallback: 'blocking' };
 }
 
 export async function getStaticProps({ params }) {
