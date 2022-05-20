c50019edca899b56346401b46769f6f4be267f02
projectlac <dft1711198@gmail.com>
2022-05-20 21:56:11 +0700

    fix fallback

--- pages/chi-tiet-tin-tuc/[id].tsx
+++ pages/chi-tiet-tin-tuc/[id].tsx
@@ -9,7 +9,7 @@ function DetailNewsPage({ post }) {
     <Layout>
       <>
         <Head>
-          <title>{post.title}</title>
+          <title>{post?.title || "Tempest"}</title>
           <meta
             name="viewport"
             content="initial-scale=1.0, width=device-width"
@@ -19,7 +19,7 @@ function DetailNewsPage({ post }) {
             content={`https://www.tempest.nv/chi-tiet-tin-tuc/${post.slug}`}
           />
           <meta property="og:type" content="article" />
-          <meta property="og:title" content={post.title} />
+          <meta property="og:title" content={post?.title || "Tempest"} />
           <meta property="og:description" content={post.description} />
           <meta property="og:image" content={post.imageUrl} />
           <meta property="og:image:width" content="1200" />
