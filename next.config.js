7a57c37baea61938a43ed339abbc6b6547e38072
projectlac <dft1711198@gmail.com>
2022-06-16 00:00:04 +0700

    site map

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./scripts/sitemap-generator");
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
---
+++ scripts/sitemap-generator.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const globby = require('globby')
+function addPage(page) {
+  const path = page.replace('pages', '').replace('.js', '').replace('.mdx', '')
+  const route = path === '/index' ? '' : path
+  return `  <url>
+    <loc>${`http://localhost:3000${route}`}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>1.0</priority>
+  </url>`
+}
+
+async function generateSitemap() {
+  // excludes Nextjs files and API routes.
+  const pages = await globby([
+    'pages/**/*{.jsx,.mdx}',
+    'pages/**/*{.tsx,.mdx}',
+    'pages/**/*{.ts,.mdx}',
+
+    '!pages/dashboard/**/*{.jsx,.mdx}',
+    '!pages/dashboard/**/*{.tsx,.mdx}',
+    '!pages/dashboard/**/*{.ts,.mdx}',
+
+
+
+    '!pages/_*.jsx',
+    '!pages/_*.tsx',
+    '!pages/_*.ts',
+
+    '!pages/api',
+  ])
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${pages.map(addPage).join('\n')}
+</urlset>`
+  fs.writeFileSync('public/sitemap.xml', sitemap)
+}
+generateSitemap()
