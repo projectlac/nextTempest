6b136d3671a0caeba6d9d5db8fdec1da07092163
projectlac <dft1711198@gmail.com>
2022-06-15 22:14:22 +0700

    add gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,16 +29,17 @@ export default class MyDocument extends Document {
             httpEquiv="Content-Security-Policy"
             content="upgrade-insecure-requests"
           ></meta>
-
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-8SZQ8DYEBH"
+          ></Script>
           <Script id="google-analytics" strategy="afterInteractive">
             {`
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'G-8SZQ8DYEBH', 'auto');
-          ga('send', 'pageview');
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-8SZQ8DYEBH');
         `}
           </Script>
         </Head>
