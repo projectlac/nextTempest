aa3fda9efaabfe69faf684d3314776b4f1f9f215
projectlac <dft1711198@gmail.com>
2022-06-15 21:52:51 +0700

    gg tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
 
 import createEmotionCache from "../utility/createEmotionCache";
+import Script from "next/script";
 
 export default class MyDocument extends Document {
   render() {
@@ -28,6 +29,18 @@ export default class MyDocument extends Document {
             httpEquiv="Content-Security-Policy"
             content="upgrade-insecure-requests"
           ></meta>
+
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'G-8SZQ8DYEBH', 'auto');
+          ga('send', 'pageview');
+        `}
+          </Script>
         </Head>
         <body>
           <Main />
