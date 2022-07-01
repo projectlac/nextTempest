3d59073691edacaf4d57059a5a4c7e5cedf0f0d5
Harry Nguyen <harry.nguyen2@techvify.com.vn>
2022-07-01 09:55:35 +0700

    Update avatar

--- data/avatar.ts
+++ data/avatar.ts
@@ -1,20 +1,29 @@
-import Avatar from "../styles/assets/images/payment/avatar-cute-12.jpg";
-import Hutao from "../styles/assets/images/Avatar/hutao.jpg";
-import Aether from "../styles/assets/images/Avatar/aether.jpg";
-import Albedo from "../styles/assets/images/Avatar/albedo.png";
-import Diona from "../styles/assets/images/Avatar/diona.png";
-import Fischl from "../styles/assets/images/Avatar/fishel.jpg";
-import Goro from "../styles/assets/images/Avatar/goro.png";
-import Lisa from "../styles/assets/images/Avatar/lisa.png";
-import Mona from "../styles/assets/images/Avatar/mona.png";
-import Noelle from "../styles/assets/images/Avatar/noelle.png";
-import Qiqi from "../styles/assets/images/Avatar/qiqi.jpg";
-import Razor from "../styles/assets/images/Avatar/razor.png";
-import Sucrose from "../styles/assets/images/Avatar/sucrose.png";
-import Xiao from "../styles/assets/images/Avatar/xiao.png";
-import Yomiya from "../styles/assets/images/Avatar/yomiya.png";
-import Jean from "../styles/assets/images/Avatar/jean.png";
-import Xingqiu from "../styles/assets/images/Avatar/xingqiu.png";
+import A1 from "../styles/assets/images/Avatar/1.png";
+import A2 from "../styles/assets/images/Avatar/2.png";
+import A3 from "../styles/assets/images/Avatar/3.png";
+import A4 from "../styles/assets/images/Avatar/4.png";
+import A5 from "../styles/assets/images/Avatar/5.png";
+import A6 from "../styles/assets/images/Avatar/6.png";
+import A7 from "../styles/assets/images/Avatar/7.png";
+import A8 from "../styles/assets/images/Avatar/8.png";
+import A9 from "../styles/assets/images/Avatar/9.png";
+import A10 from "../styles/assets/images/Avatar/10.png";
+import A11 from "../styles/assets/images/Avatar/11.png";
+import A12 from "../styles/assets/images/Avatar/12.png";
+import A13 from "../styles/assets/images/Avatar/13.jpg";
+import A14 from "../styles/assets/images/Avatar/14.png";
+import A15 from "../styles/assets/images/Avatar/15.png";
+import A16 from "../styles/assets/images/Avatar/16.png";
+import A17 from "../styles/assets/images/Avatar/17.png";
+import A18 from "../styles/assets/images/Avatar/18.png";
+import A19 from "../styles/assets/images/Avatar/19.png";
+import A20 from "../styles/assets/images/Avatar/20.png";
+import A21 from "../styles/assets/images/Avatar/21.png";
+import A22 from "../styles/assets/images/Avatar/22.png";
+import A23 from "../styles/assets/images/Avatar/23.png";
+import A24 from "../styles/assets/images/Avatar/24.png";
+import A25 from "../styles/assets/images/Avatar/25.png";
+
 interface AvatarDecor {
   id: number;
   url: string;
@@ -22,67 +31,103 @@ interface AvatarDecor {
 const avatar: AvatarDecor[] = [
   {
     id: 1,
-    url: Aether.src,
+    url: A1.src,
   },
   {
     id: 2,
-    url: Albedo.src,
+    url: A2.src,
   },
   {
     id: 3,
-    url: Fischl.src,
+    url: A3.src,
   },
   {
     id: 4,
-    url: Diona.src,
+    url: A4.src,
   },
   {
     id: 5,
-    url: Goro.src,
+    url: A5.src,
   },
   {
     id: 6,
-    url: Lisa.src,
+    url: A6.src,
   },
   {
     id: 7,
-    url: Mona.src,
+    url: A7.src,
   },
   {
     id: 8,
-    url: Noelle.src,
+    url: A8.src,
   },
   {
     id: 9,
-    url: Qiqi.src,
+    url: A9.src,
   },
   {
     id: 10,
-    url: Razor.src,
+    url: A10.src,
   },
   {
     id: 11,
-    url: Sucrose.src,
+    url: A11.src,
   },
   {
     id: 12,
-    url: Xiao.src,
+    url: A12.src,
   },
   {
     id: 13,
-    url: Yomiya.src,
+    url: A13.src,
   },
   {
     id: 14,
-    url: Hutao.src,
+    url: A14.src,
   },
   {
     id: 15,
-    url: Jean.src,
+    url: A15.src,
   },
   {
     id: 16,
-    url: Xingqiu.src,
+    url: A16.src,
+  },
+  {
+    id: 17,
+    url: A17.src,
+  },
+  {
+    id: 18,
+    url: A18.src,
+  },
+  {
+    id: 19,
+    url: A19.src,
+  },
+  {
+    id: 20,
+    url: A20.src,
+  },
+  {
+    id: 21,
+    url: A21.src,
+  },
+  {
+    id: 22,
+    url: A22.src,
+  },
+  {
+    id: 23,
+    url: A23.src,
+  },
+  {
+    id: 24,
+    url: A24.src,
+  },
+  {
+    id: 25,
+    url: A25.src,
   },
 ];
 export default avatar;
