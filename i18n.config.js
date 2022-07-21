0e183e14f9dac61a5956d706c2052b54224ae475
projectlac <dft1711198@gmail.com>
2022-07-21 23:56:09 +0700

    push multi language

--- i18n.config.js
+++ i18n.config.js
@@ -1,36 +1,20 @@
-module.exports = {
+const configLanguage = {
   i18n: {
-    // These are all the locales you want to support in
-    // your application
-    locales: ["", "en", "vi"],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: "",
-    // This is a list of locale domains and the default locale they
-    // should handle (these are only required when setting up domain routing)
+    locales: ["en", "vi"],
+    defaultLocale: "vi",
     domains: [
       {
+        // Note: subdomains must be included in the domain value to be matched
+        // e.g. www.example.com should be used if that is the expected hostname
         domain: "www.tempest.vn",
         defaultLocale: "vi",
-        // other locales that should be handled on this domain
-        locales: [""],
-      },
-      {
-        domain: "tempest.vn",
-        defaultLocale: "vi",
-        // other locales that should be handled on this domain
-        locales: ["vi"],
       },
       {
         domain: "www.tempest.vn/en",
         defaultLocale: "en",
         locales: ["en"],
       },
-      {
-        domain: "tempest.vn/en",
-        defaultLocale: "en",
-        locales: ["en"],
-      },
     ],
   },
 };
+module.exports = configLanguage;
