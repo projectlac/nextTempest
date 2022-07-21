0aa03910c3e568d10c05dcf33a3d48a627f768b7
projectlac <dft1711198@gmail.com>
2022-07-21 23:48:48 +0700

    push multi language

--- i18n.config.js
+++ i18n.config.js
@@ -1,25 +1,36 @@
 module.exports = {
-    i18n: {
-      // These are all the locales you want to support in
-      // your application
-      locales: ['en', 'vi'],
-      // This is the default locale you want to be used when visiting
-      // a non-locale prefixed path e.g. `/hello`
-      defaultLocale: 'vi',
-      // This is a list of locale domains and the default locale they
-      // should handle (these are only required when setting up domain routing)
-      domains: [
-        {
-          domain: 'www.tempest.vn',
-          defaultLocale: 'vi',
-          // other locales that should be handled on this domain
-          locales: ['vi'],
-        },
-        {
-          domain: 'www.tempest.vn/en',
-          defaultLocale: 'en',
-        },
-        
-      ],
-    },
-  }
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ["", "en", "vi"],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: "",
+    // This is a list of locale domains and the default locale they
+    // should handle (these are only required when setting up domain routing)
+    domains: [
+      {
+        domain: "www.tempest.vn",
+        defaultLocale: "vi",
+        // other locales that should be handled on this domain
+        locales: [""],
+      },
+      {
+        domain: "tempest.vn",
+        defaultLocale: "vi",
+        // other locales that should be handled on this domain
+        locales: ["vi"],
+      },
+      {
+        domain: "www.tempest.vn/en",
+        defaultLocale: "en",
+        locales: ["en"],
+      },
+      {
+        domain: "tempest.vn/en",
+        defaultLocale: "en",
+        locales: ["en"],
+      },
+    ],
+  },
+};
