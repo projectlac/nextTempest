077ebec3fe3eb8dfae14f8cf5d7c3ce3cf0da0f9
Harry Nguyen <harry.nguyen2@techvify.com.vn>
2022-07-22 08:43:20 +0700

    test env

--- i18n.config.js
+++ i18n.config.js
@@ -2,19 +2,6 @@ const configLanguage = {
   i18n: {
     locales: ["en", "vi"],
     defaultLocale: "vi",
-    domains: [
-      {
-        // Note: subdomains must be included in the domain value to be matched
-        // e.g. www.example.com should be used if that is the expected hostname
-        domain: "www.tempest.vn",
-        defaultLocale: "vi",
-      },
-      {
-        domain: "www.tempest.vn",
-        defaultLocale: "en",
-        locales: ["en"],
-      },
-    ],
   },
 };
 module.exports = configLanguage;
