6f29970dca2d21ec4ddcc8fba49d796c64b389c8
projectlac <dft1711198@gmail.com>
2022-09-26 20:25:15 +0700

    change font

--- utility/lang/en.ts
+++ utility/lang/en.ts
@@ -1,17 +1,38 @@
+// const home = [
+//   [
+//     `Tempest Shop where provide accounts,`,
+//     "Safely top up games best in Vietnam",
+//   ],
+//   [
+//     "Home",
+//     "Buy account",
+//     "News",
+//     "Payment",
+//     "Contact",
+//     "Sign In",
+//     "Sign Out",
+//     "Sign Up",
+//   ],
+// ];
+// export default home;
 const home = [
   [
-    `Tempest Shop where provide accounts,`,
-    "Safely top up games best in Vietnam",
+    `Tempest Shop nơi cung cấp tài khoản Genshin, Tower of Fantasy`, // home
+    "an toàn nhất Việt Nam",
   ],
+
+  // menu
+
   [
-    "Home",
-    "Buy account",
-    "News",
-    "Payment",
-    "Contact",
-    "Sign In",
-    "Sign Out",
-    "Sign Up",
+    "Trang chủ",
+    "Mua tài khoản",
+    "Tin tức",
+    "Nạp coin",
+    "Liên hệ",
+    "Đăng nhập",
+    "Đăng xuất",
+    "Đăng ký",
   ],
 ];
+
 export default home;
