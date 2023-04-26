0d80b2510143d0ef6382e5c4ab6249f95926a06c
projectlac <dft1711198@gmail.com>
2023-04-26 22:08:57 +0700

    image

--- next.config.js
+++ next.config.js
@@ -4,7 +4,12 @@ const { i18n } = require("./i18n.config");
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["res.cloudinary.com", "imagedelivery.net"],
+    domains: [
+      "res.cloudinary.com",
+      "imagedelivery.net",
+      "upanh.tv",
+      "img.upanh.tv",
+    ],
   },
   i18n,
 };
