e0515ec87654e3a73b5d299632ad704da5ebbd57
Sotatek-TuyenDang2 <tuyen.dang2@sotatek.com>
2023-08-16 17:51:10 +0700

    Change UT

--- components/Common/Menu/MenuBox.tsx
+++ components/Common/Menu/MenuBox.tsx
@@ -181,9 +181,9 @@ function MenuBox({ activeMenu, closeMenu, login, logout }: PropsMenu) {
             <Typography className={`${activeClass("/lien-he")}`}>
               <Link href="/lien-he">{trans[1][4]}</Link>
             </Typography>
-            <Typography className={`${activeClass("/uy-tin")}`}>
+            {/* <Typography className={`${activeClass("/uy-tin")}`}>
               <Link href="/uy-tin">Check UT</Link>
-            </Typography>
+            </Typography> */}
             {["ADMIN", "MOD", "CHECKED"].includes(role) && isLogin && (
               <Typography component="h2">
                 <Link href="/dashboard">Quản lý</Link>
