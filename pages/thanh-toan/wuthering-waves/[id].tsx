a2be1c7cf838e44e4f355f9485c33d89d3c9ea67
projectlac <dft1711198@gmail.com>
2024-05-25 23:24:54 +0700

    update thanh tona

---
+++ pages/thanh-toan/wuthering-waves/[id].tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import * as React from "react";
+import Layout from "../../../components/Layout/BaseLayout";
+import SubmitBuy from "../../../components/Modules/SubmitBuy/SubmitBuy";
+
+export default function IndexPage() {
+  const router = useRouter();
+  const { id, redirect } = router.query;
+
+  return (
+    <Layout>
+      <Head>
+        <title>
+          Thanh toán - Tempest Shop - Mua bán tài khoản Genshin Impact, Tower of
+          fantasy uy tín hàng đầu Việt Nam
+        </title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <SubmitBuy ids={id as string} slug={redirect as string} />
+    </Layout>
+  );
+}
+
+IndexPage.getLayout = function getLayout(page) {
+  return <Layout>{page}</Layout>;
+};
