dcdb0cca3b9526e90edaff7844856b2c3cac5732
projectlac <dft1711198@gmail.com>
2024-07-04 09:57:00 +0700

    update reroll

--- components/Layout/BaseLayout.tsx
+++ components/Layout/BaseLayout.tsx
@@ -1,10 +1,11 @@
-import Head from "next/head";
 import HeaderHome from "../Common/Header/HeaderHome";
 // import { MessengerChat, showMessenger } from "react-messenger-chat-plugin";
-import Social from "../Common/Social/Social";
-import MessengerChat from "react-messenger-customer-chat";
+import { Box } from "@mui/material";
+import Image from "next/image";
 import Footer from "../Common/Footer/Footer";
+import Social from "../Common/Social/Social";
 import Walker from "../Common/Walker/Walker";
+import Fb from "../../styles/assets/images/fb_icon.svg";
 export default function Layout({ children }) {
   return (
     <>
@@ -18,8 +19,26 @@ export default function Layout({ children }) {
         language="vi_VN"
       /> */}
       <Walker />
-      <MessengerChat pageId="103539711584646" language="vi_VN" />
-
+      {/* <MessengerChat pageId="103539711584646" language="vi_VN" /> */}
+      <Box
+        sx={{
+          width: 90,
+          height: 90,
+          position: "fixed",
+          right: "-5px",
+          bottom: "0px",
+        }}
+      >
+        <a href={"https://m.me/103539711584646"} target="__blank">
+          <Image
+            src={Fb}
+            alt="Facebook"
+            width={60}
+            height={60}
+            objectFit="contain"
+          />
+        </a>
+      </Box>
       <Social />
       <Footer />
     </>
